53cdf6c896e3f2121cf0fc1cfc75dd7696592b65
voidalzk <voidalzk@gmail.com>
2025-03-16 16:36:04 -0300

    feat: add Organizations component with initial structure and tests

---
+++ src/app/components/organizations/organizations.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-organizations',
+  standalone: true,
+  imports: [],
+  templateUrl: './organizations.component.html',
+  styleUrl: './organizations.component.css'
+})
+export class OrganizationsComponent {
+
+}
